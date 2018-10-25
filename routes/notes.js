1e75f2321ed23c8d2bccb0d9bf1bcbd0e1b39ff6
Terry Shenker <tshenker@gmail.com>
2018-10-25 18:28:24 +0900

    added notes page

--- routes/notes.js
+++ routes/notes.js
@@ -6,6 +6,45 @@ const mongoose = require('mongoose');
 require('../models/Note');
 const Note = mongoose.model('notes');
 
+router.get('/all', (req, res) => {
+    pageName = "Project notes";
+    noteList = [
+        {
+            name: 'Note1',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            createdOn: '11 October 2018',
+            status: 'WIP'
+        },
+        {
+            name: 'Note2',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            createdOn: '12 October 2018',
+            status: 'Under review'
+        },
+        {
+            name: 'Note3',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            createdOn: '24 October 2018',
+            status: 'Not started'
+        },
+                {
+            name: 'Note4',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            createdOn: '12 October 2018',
+            status: 'WIP'
+        },
+        {
+            name: 'Note5',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            createdOn: '24 October 2018',
+            status: 'Completed'
+        }
+    ]
+    res.render('notes', {
+        pageName: pageName,
+        noteList: noteList
+    });
+})
 
 router.get('/new', (req, res) => {
     res.send('New note');
