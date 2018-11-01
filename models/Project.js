29b0828241b543c33c017a0694aa3921b2354e30
Terry <tshenker@gmail.com>
2018-11-01 08:39:30 +0000

    editing a project

--- models/Project.js
+++ models/Project.js
@@ -4,49 +4,17 @@ const Schema = mongoose.Schema;
 let projectSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
-        unique: true
+        required: true
+        // unique: true
     },
     details: {
         type: String,
         required: true
     },
-    user: {
-        type: String,
-        required:true
-    },
     date: {
-        type: Date,
-        default: Date.now
-    }
-    /*
-    TODO: review project schema
-    projectName: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    projectCreatedOn: {
         type: Date,
         'default': Date.now
     },
-    projectModifiedOn: {
-        type: Date,
-        default: Date.now
-    },
-    projectCreatedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    // projectNotes: [noteSchema],
-    projectActiveStatus: {
-        type: Boolean,
-        default: true
-    }
-    */
 });
-
-// TODO: create noteSchema
-// TODO: at timeline & milestones
 
 mongoose.model('projects', projectSchema);
--- routes/projects.js
+++ routes/projects.js
@@ -19,76 +19,16 @@ const protectRoute = (req, res, next) => {
 // Dashboard route
 router.get('/dashboard', (req, res) => {
     const pageName = "Dashboard";
-    // TODO: delete dummy and get from database
-    // TODO: create add project button next to login
-    let projectList = [
-        {
-            projectName: 'Project1',
-            startDate: '22 October 2018',
-            endDate: '1 April 2020',
-            numNotes: 10
-        },
-        {
-            projectName: 'Project2',
-            startDate: '2 October 2018',
-            endDate: '12 April 2020',
-            numNotes: 7
-        },
-        {
-            projectName: 'Project3',
-            startDate: '2 September 2018',
-            endDate: '12 January 2020',
-            numNotes: 8
-        },
-        {
-            projectName: 'Project4',
-            startDate: '12 July 2018',
-            endDate: '12 June 2020',
-            numNotes: 2
-        },
-        {
-            projectName: 'Project5',
-            startDate: '12 April 2018',
-            endDate: '2 June 2019',
-            numNotes: 6
-        },
-        {
-        projectName: 'Project6',
-            startDate: '12 April 2018',
-            endDate: '2 June 2019',
-            numNotes: 11
-        },
-        {
-            projectName: 'Project7',
-            startDate: '12 April 2018',
-            endDate: '2 June 2019',
-            numNotes: 9
-        },
-        {
-            projectName: 'Project8',
-            startDate: '12 April 2018',
-            endDate: '2 June 2019',
-            numNotes: 3
-        },
-        {
-            projectName: 'Project9',
-            startDate: '12 April 2018',
-            endDate: '2 June 2019',
-            numNotes: 12
-        },
-        {
-            projectName: 'Project10',
-            startDate: '12 April 2018',
-            endDate: '2 June 2019',
-            numNotes: 4
-        }
-    ];
-    res.render('projects/dashboard', {
-        pageName: pageName,
-        functionBarRoute: '/projects/add',
-        functionBarLabel: 'Project',
-        projectList: projectList
-    });
+    Project.find({})
+        .sort({ date: 'desc' })
+        .then(project => {
+            res.render('projects/dashboard', {
+                pageName: pageName,
+                functionBarRoute: '/projects/add',
+                functionBarLabel: 'Project',
+                projectList: project
+    });  
+        })
 });
 
 router.get('/add', (req, res) => {
@@ -133,8 +73,30 @@ router.post('/add', (req, res) => {
         })
     }
     else {
-        res.send("Added a project")
+        const newProject = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Project(newProject)
+            .save()
+            .then(idea => {
+                res.redirect('/projects/dashboard')
+            })
     }
+});
+
+router.get('/edit:id', (req, res) => {
+    const pageName = 'Edit project';
+    Project.findOne({
+        _id: req.params.id 
+    })
+        .then(project => {
+            res.render('projects/edit', {
+                pageName: pageName,
+                errors: null,
+                project: project
+            }); 
+        });
 });
 
 module.exports = router;
